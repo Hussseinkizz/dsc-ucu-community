ce01b086381fff156fe84ef83e4942d882ee6306
Hussseinkizz <hssnkizz@gmail.com>
2023-04-03 01:55:33 +0300

    Feat: useJsonServer

---
+++ src/hooks/useJsonServer.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useJsonServer = (resourceUrl) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchAll();
+  }, []);
+
+  const fetchAll = async () => {
+    try {
+      const response = await axios.get(resourceUrl);
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const create = async (newData) => {
+    try {
+      const response = await axios.post(resourceUrl, newData);
+      setData([...data, response.data]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const update = async (id, updatedData) => {
+    try {
+      const response = await axios.put(`${resourceUrl}/${id}`, updatedData);
+      const updatedIndex = data.findIndex((item) => item.id === id);
+      const updatedArray = [...data];
+      updatedArray[updatedIndex] = response.data;
+      setData(updatedArray);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const remove = async (id) => {
+    try {
+      await axios.delete(`${resourceUrl}/${id}`);
+      const filteredArray = data.filter((item) => item.id !== id);
+      setData(filteredArray);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { data, create, update, remove };
+};
+
+export default useJsonServer;
